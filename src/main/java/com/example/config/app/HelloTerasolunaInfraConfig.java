1db050b1882fb32e834322a0bfc6a757e9dd97b4
Toshiaki Maki <makingx@gmail.com>
2024-04-22 12:59:38 +0900

    first commit

---
+++ src/main/java/com/example/config/app/HelloTerasolunaInfraConfig.java
@@ -0,0 +1,11 @@
+package com.example.config.app;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Bean definitions for infrastructure layer.
+ */
+@Configuration
+public class HelloTerasolunaInfraConfig {
+
+}
